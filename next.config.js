b4a47ebf588e4e59f179bee1b990d67afebba3b9
tore <maybeguy1998@gmail.com>
2023-01-17 14:09:11 +0100

    fix: base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-   basePath: '/docs',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  basePath: '/docs',
+})
